d08d2be70f01962607092cc33033a4bdd30f2a76
Ozie Virto <ovirto@outlook.com>
2024-11-15 00:47:40 -0600

    Comments added to Main.java

--- HiWorld/src/Main.java
+++ HiWorld/src/Main.java
@@ -1,5 +1,10 @@
+// Good program bb! You made your first repo!
+
+// Tomorrow we can make a new push for your actual program
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hi world!");
     }
 }
